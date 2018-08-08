fe14281bfe45b0f235e4eb6b96d5f35c28cea51c
Bart van Kuik <bart@vankuik.nl>
2018-08-08 11:06:09 +0100

    Renamed project

--- ViewWithRoundedAndShadowedCorners/ViewWithRoundedAndShadowedCorners.swift
+++ ViewWithRoundedAndShadowedCorners/ViewWithRoundedAndShadowedCorners.swift
@@ -1,6 +1,6 @@
 //
 //  ViewWithRoundedAndShadowedCorners.swift
-//  CustomViewWithShadow
+//  ViewWithRoundedAndShadowedCorners
 //
 //  Created by Bart van Kuik on 08/08/2018.
 //  Copyright © 2018 DutchVirtual. All rights reserved.
